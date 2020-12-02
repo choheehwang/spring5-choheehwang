2f834f665a2e222e5599aeace7f98d5451cc50bb
user <user@DESKTOP-6235SCL>
2020-12-02 12:49:52 +0900

    u

--- src/main/java/org/edu/controller/AdminController.java
+++ src/main/java/org/edu/controller/AdminController.java
@@ -1,5 +1,14 @@
 package org.edu.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+//spring에서 사용 가능한 클래스를 bean이라 한다
+@Controller
 public class AdminController {
 
+	@RequestMapping(value="/admin", method=RequestMethod.GET)
+	public String admin() {
+		return "admin/home";
+	}
 }
