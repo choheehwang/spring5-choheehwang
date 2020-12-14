36c77c5b5bba69d900c7ed78f254f96bfafe68cc
user <user@DESKTOP-6235SCL>
2020-12-14 21:30:24 +0900

    up

--- src/test/java/kr/or/member/MemberVO.java
+++ src/test/java/kr/or/member/MemberVO.java
@@ -2,9 +2,13 @@ package kr.or.member;
 
 public class MemberVO {
 		//멤버 변수 생성(아래)
+		
 		private String name;
 		private int age;
 		private String phoneNum;
+		//getter and setter 사용 이유: 외부에서 데이터의 직접 접근을 막기 위해.
+		//getter: private 필드 값 읽기 전용, setter: private 필드 값 쓰기 전용
+		//getter and setter 없으면 외부 접근 제어
 		
 		public String getName() {
 			return name;
