7b0df69f9fd9182fde9a75884724d7f42994518f
user <user@DESKTOP-6235SCL>
2020-12-14 11:03:34 +0900

    up

---
+++ src/test/java/org/edu/test/DataSourceTest.java
@@ -0,0 +1,26 @@
+package org.edu.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * JUnit 자바단위테스트이고, DataSource 지정 후 DB(HSQL, MySQL, Oracle) 접속
+ * DB Table CRUD(Create, Read, Update, Delete) 테스트용 클래스
+ * JUnit4 class 사용
+ * @author 황초희
+ *
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class DataSourceTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		
+	}
+
+}
