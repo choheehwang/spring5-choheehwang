7445791f3a6b8846b309714c196b1489e529abf9
user <user@DESKTOP-6235SCL>
2020-12-16 00:20:54 +0900

    up

--- src/test/java/kr/or/member/MemberService.java
+++ src/test/java/kr/or/member/MemberService.java
@@ -3,19 +3,16 @@ package kr.or.member;
 import kr.or.member.MemberVO;
 
 public class MemberService {
-	public void printMember(MemberVO[] members) {// members는 레코드가 여러개=3개레코드가들어감
-		// 서비스 구현메서드 내용(아래) 향상된 for반복문 사용
-		//기존 for문: for(초기화; 조건식: 증감식){ 내용;}
-		//확장(향상)된  for문: for(변수명 : 반복대상){내용;}
+	public void printMember(MemberVO[] members) {//members는 레코드가 여러개=3개(성춘향, 홍길동, 각시탈) 레코드가 들어감
+		//서비스 구현 메서드 내용(아래) 향상된 for반복문 사용
+		//기존 for문-> for(초기화; 조건식: 증감식){ 내용;}
+		//확장(향상)된  for문-> for(변수명 : 반복대상){내용;}
 		int cnt = 0;
-		for (MemberVO member : members) {// members여러 레코드중 1개의 레코드를 member오브젝트객체로 대입
-			//여기서 members[0]=MemberVO member -> members[1]=MemberVO member -> members[2]
-			//MemberVO클래스형데이터타입을 member 변수로 만듦.
+		for (MemberVO member : members) {// members여러 레코드중 1개의 레코드를 member 오브젝트로 대입
 			cnt = cnt + 1;
+			//cnt + 1을 할 경우 결과가 "1"번째 레코드는~ 라는 결과가 나오고
+			//안 했을 경우 "0"번째 레코드는~ 라는 결과가 나오게 되므로 cnt + 1을 해준 것이다
 			System.out.println(cnt + "번째 레코드는 " + member.toString());
 		}
-		//비지니스 로직에서 처리하는 내용은=데이터를 가공한다.(아래)
-		//위처럼 데이터 select가져오기,insert입력하기,update수정하기
-		//delete삭제하기 => CRUD를 구현하는 내용
 	}
 }
--- src/test/java/kr/or/member/MemberVO.java
+++ src/test/java/kr/or/member/MemberVO.java
@@ -1,8 +1,11 @@
 package kr.or.member;
 
 public class MemberVO {
-		//멤버 변수 생성(아래)
+		//MemberVO: DB 테이블로 부터 조회한 컬럼값(데이터)을 변수에 저장하거나
+		//또는 변수에 저장된값을 VO객체를 생성하여 DB에 추가하는 역할
+		//테이블의 컬럼명과 동일한 자료형과 이름으로 변수를 선언하고, getter/setter 메소드 생성
 		
+		//멤버 변수 생성(아래)
 		private String name;
 		private int age;
 		private String phoneNum;
@@ -36,6 +39,7 @@ public class MemberVO {
 
 		@Override
 		public String toString() { //멤버 변수 값들이 출력되게끔 리턴값을 설정하는 메소드
+		//숫자와 문자열을 섞어서 출력해야하는 경우, toString()을 활용하여 확실하게 문자열로 바꾸어 주면 불필요한 오류 미리 방지 가능
 			return "디버그용 MemberVO [name=" + name + ", age=" + age + ", phoneNum=" + phoneNum + "]";
 		}
 
