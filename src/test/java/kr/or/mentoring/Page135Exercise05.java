6a582371e3af23e2876b1cd47c4d041089572166
user <user@DESKTOP-6235SCL>
2021-01-28 20:18:22 +0900

    Mentoring up

--- src/test/java/kr/or/mentoring/Page135Exercise05.java
+++ src/test/java/kr/or/mentoring/Page135Exercise05.java
@@ -6,8 +6,8 @@ public class Page135Exercise05 {
 		// 이것이 자바다 p.135 5번 예제
 		for(int x=1; x<11; x++) {
 			for(int y=1; y<11; y++) {
-				if((4*x)+(5*y) == 60) {
-					System.out.println("("+x+", "+y+")");
+				if(4*x+5*y==60) {
+					System.out.println("("+x+","+y+")");
 				}
 			}
 		}
